0810e964a76e680200ab12fed0a0c943fca93d79
KaKi87 <KaKi87@proton.me>
2025-11-08 18:00:58 +0100

    :recycle: Move duplicated code to own and refactored 'generateAndSendWheel' function & more

--- index.js
+++ index.js
@@ -3,6 +3,37 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const { createGIF } = require('./wheel');
 
+const
+    COLORS_GRADIENT = [
+        '#524135',
+        '#8c643c',
+        '#a07955',
+        '#c49c6c',
+        '#bc966c'
+    ],
+    getColorByIndex = idx => COLORS_GRADIENT[idx % COLORS_GRADIENT.length],
+    generateAndSendWheel = async (
+        interaction,
+        options,
+        {
+            replyMethod = 'reply',
+            template = winner => `The winner is **${winner}**!`
+        } = {}
+    ) => {
+        const winnerOption = options[Math.floor(Math.random() * options.length)];
+        winnerOption.winner = true;
+        await interaction[replyMethod](`Generating wheel with ${options.length} options...`);
+        const gif = await createGIF(options);
+        await interaction.editReply({
+            files: [{
+                attachment: gif,
+                name: 'wheel.gif'
+            }]
+        });
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        await interaction.editReply({ content: template(winnerOption.label) });
+    };
+
 const client = new Discord.Client({
     intents: [Discord.IntentsBitField.Flags.Guilds, Discord.IntentsBitField.Flags.GuildVoiceStates, Discord.IntentsBitField.Flags.GuildPresences, Discord.IntentsBitField.Flags.GuildMembers]
 });
@@ -33,92 +64,28 @@ client.on('interactionCreate', async (interaction) => {
             }
             console.log(users, users.flat());
 
-            const colorsGradient = [
-                '#524135',
-                '#8c643c',
-                '#a07955',
-                '#c49c6c',
-                '#bc966c'
-            ];
             const options = Array.from(new Set(users.flat().map((u) => u.id))).map((opt, idx) => ({
                 label: client.users.cache.get(opt)?.username,
-                color: colorsGradient[idx % colorsGradient.length]
+                color: getColorByIndex(idx)
             }));
 
-            const winnerOption = options[Math.floor(Math.random() * options.length)];
-            const winnerIndex = options.indexOf(winnerOption);
-            options[winnerIndex] = {
-                ...winnerOption,
-                winner: true
-            };
-
-            await interaction.followUp(`Generating wheel with ${options.length} options...`);
-            
-            createGIF(options).then(async (gif) => {
-
-                // send
-                await interaction.editReply({
-                    files: [{
-                        attachment: gif,
-                        name: 'wheel.gif'
-                    }]
-                });
-
-                setTimeout(() => {
-                    interaction.editReply({
-                        content: `The winner is **${winnerOption.label}**!`
-                    });
-                }, 5000);
-
-            })
+            await generateAndSendWheel(interaction, options, { replyMethod: 'followUp' });
 
         }
 
         if (interaction.commandName === 'wheel-voice') {
-            
+
             const voiceMembers = interaction.member.voice?.channel?.members;
             if (!voiceMembers) {
                 return interaction.reply('You must be in a voice channel to use this command!');
             }
 
-            const colorsGradient = [
-                '#524135',
-                '#8c643c',
-                '#a07955',
-                '#c49c6c',
-                '#bc966c'
-            ];
             const options = voiceMembers.filter((m) => !m.user.bot).map((opt, idx) => ({
                 label: opt.user?.username,
-                color: colorsGradient[idx % colorsGradient.length]
+                color: getColorByIndex(idx)
             }));
 
-            const winnerOption = options[Math.floor(Math.random() * options.length)];
-            const winnerIndex = options.indexOf(winnerOption);
-            options[winnerIndex] = {
-                ...winnerOption,
-                winner: true
-            };
-
-            await interaction.reply(`Generating wheel with ${options.length} options...`);
-            
-            createGIF(options).then(async (gif) => {
-
-                // send
-                await interaction.editReply({
-                    files: [{
-                        attachment: gif,
-                        name: 'wheel.gif'
-                    }]
-                });
-
-                setTimeout(() => {
-                    interaction.editReply({
-                        content: `The winner is **${winnerOption.label}**!`
-                    });
-                }, 5000);
-
-            })
+            await generateAndSendWheel(interaction, options);
 
         }
 
@@ -128,44 +95,12 @@ client.on('interactionCreate', async (interaction) => {
                 withPresences: true
             });
 
-            const colorsGradient = [
-                '#524135',
-                '#8c643c',
-                '#a07955',
-                '#c49c6c',
-                '#bc966c'
-            ];
             const options = interaction.guild.members.cache.filter((m) => m.presence && m.presence?.status !== 'invisible' && !m.user.bot) .map((opt, idx) => ({
                 label: opt.user.username,
-                color: colorsGradient[idx % colorsGradient.length]
+                color: getColorByIndex(idx)
             }));
 
-            const winnerOption = options[Math.floor(Math.random() * options.length)];
-            const winnerIndex = options.indexOf(winnerOption);
-            options[winnerIndex] = {
-                ...winnerOption,
-                winner: true
-            };
-
-            await interaction.reply(`Generating wheel with ${options.length} options...`);
-            
-            createGIF(options).then(async (gif) => {
-
-                // send
-                await interaction.editReply({
-                    files: [{
-                        attachment: gif,
-                        name: 'wheel.gif'
-                    }]
-                });
-
-                setTimeout(() => {
-                    interaction.editReply({
-                        content: `**THE WINNER IS ${winnerOption.label.toUpperCase()}**!`
-                    });
-                }, 5000);
-
-            })
+            await generateAndSendWheel(interaction, options, { template: winner => `**THE WINNER IS ${winner.toUpperCase()}**!` });
 
         }
 
@@ -175,87 +110,23 @@ client.on('interactionCreate', async (interaction) => {
                 withPresences: true
             });
 
-            const colorsGradient = [
-                '#524135',
-                '#8c643c',
-                '#a07955',
-                '#c49c6c',
-                '#bc966c'
-            ];
             const options = interaction.guild.members.cache.filter((m) => m.roles.cache.has('1029342182613725246') && !m.user.bot).map((opt, idx) => ({
                 label: opt.user.username,
-                color: colorsGradient[idx % colorsGradient.length]
+                color: getColorByIndex(idx)
             }));
 
-            const winnerOption = options[Math.floor(Math.random() * options.length)];
-            const winnerIndex = options.indexOf(winnerOption);
-            options[winnerIndex] = {
-                ...winnerOption,
-                winner: true
-            };
-
-            await interaction.reply(`Generating wheel with ${options.length} options...`);
-            
-            createGIF(options).then(async (gif) => {
-
-                // send
-                await interaction.editReply({
-                    files: [{
-                        attachment: gif,
-                        name: 'wheel.gif'
-                    }]
-                });
-
-                setTimeout(() => {
-                    interaction.editReply({
-                        content: `**THE WINNER IS ${winnerOption.label.toUpperCase()}**!`
-                    });
-                }, 5000);
-
-            })
+            await generateAndSendWheel(interaction, options, { template: winner => `**THE WINNER IS ${winner.toUpperCase()}**!` });
 
         }
 
         if (interaction.commandName === 'wheel') {
 
-            const colorsGradient = [
-                '#524135',
-                '#8c643c',
-                '#a07955',
-                '#c49c6c',
-                '#bc966c'
-            ];
             const options = interaction.options.getString('options').split(',').map((opt, idx) => ({
                 label: opt,
-                color: colorsGradient[idx % colorsGradient.length]
+                color: getColorByIndex(idx)
             }));
 
-            const winnerOption = options[Math.floor(Math.random() * options.length)];
-            const winnerIndex = options.indexOf(winnerOption);
-            options[winnerIndex] = {
-                ...winnerOption,
-                winner: true
-            };
-
-            await interaction.reply(`Generating wheel with ${options.length} options...`);
-            
-            createGIF(options).then(async (gif) => {
-
-                // send
-                await interaction.editReply({
-                    files: [{
-                        attachment: gif,
-                        name: 'wheel.gif'
-                    }]
-                });
-
-                setTimeout(() => {
-                    interaction.editReply({
-                        content: `The winner is **${winnerOption.label}**!`
-                    });
-                }, 5000);
-
-            })
+            await generateAndSendWheel(interaction, options);
         }
 
     }
